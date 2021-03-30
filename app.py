9b90cd758586a43a6dea68a77bf324e3d37b1d2c
kuzanna2016 <31651155+kuzanna2016@users.noreply.github.com>
2021-03-30 11:00:44 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ import os
 from flask import Flask, request, render_template, Response
 from collections import defaultdict
 from neo4j import GraphDatabase, basic_auth
+import json
 app = Flask(__name__)
+
+
+features = {'Word': ['lemma', 'pos'],
+            'Meaning': ['id', 'Top5'],
+            'IS_HYPONYM_OF': ['freq'],
+            'HAS_MEANING': []}
 
 
 @app.route('/')
@@ -12,8 +19,15 @@ def index():
         action = parse_action(req)
         arguments = parse_arguments(req)
         result = action(arguments)
+        result = postprocc_result(result)
         return render_template('index.html', result=result)
     return render_template('index.html')
+
+
+def postprocc_result(result):
+    if result:
+        result = result.data()
+    return json.dumps(result, ensure_ascii=False)
 
 
 def parse_action(req):
@@ -36,6 +50,7 @@ def parse_arguments(req):
         if '-' in entity:
             node, label = entity.split('-')
             args['label'] = label
+            args['entity'] = node
         else:
             args['entity'] = entity
         args[value_name] = v
@@ -43,43 +58,84 @@ def parse_arguments(req):
 
 
 def delete(arg_dict):
-    # TODO: cypher delete
-    result=''
-
+    node_type = arg_dict.get('type', 'Word')
+    node_prop = features[node_type][0]
+    node_value = arg_dict['value'] if node_prop == 'id' else f"'{arg_dict['value']}'"
+    prop = arg_dict['prop']
+    if prop == '_':
+        req = "MATCH (x:" + node_type + " {" + node_prop + ": " + node_value + "})\n" + \
+              "DETACH DELETE x"
+    else:
+        req = "MATCH (x:" + node_type + " {" + node_prop + ": " + node_value + "})\n" + \
+              "REMOVE x." + prop
+    result = session.run(req)
     return result
 
 
 def create(arg_dict):
-    # TODO: cypher create
-    result = ''
-    features = ''
-    for k, v in arg_dict.items():
-        if k == 'label' or k == 'entity':
-            continue
-        features += k + ': "' + v + '", '
-    req = "CREATE (:" + arg_dict['label'] + " {" + features + "})"
-    # result = session.run(req)
-    return req
+    result = []
+    params = []
+    ent = arg_dict.get('entity')
+    if ent is not None and ent == 'node':
+        for k, v in arg_dict.items():
+            if k == 'label' or k == 'entity':
+                continue
+            elif k == 'id':
+                params.append(k + ': ' + v)
+            else:
+                params.append(k + ': "' + v + '"')
+        req = "CREATE (r:" + arg_dict['label'].capitalize() + " {" + ", ".join(params) + "})"
+        req += "\nRETURN r"
+        result = session.run(req)
+    elif ent is not None and ent == 'rel':
+        prop_value = arg_dict.get('prop-value')
+        if 'MEANING' in arg_dict['type']:
+            req = "MATCH (s:Word {lemma: '" + arg_dict['source'] + "'})\n" + \
+                  "MATCH (t:Meaning {id: " + arg_dict['target'] + "})\n"
+        else:
+            req = "MATCH (s:Word {lemma: '" + arg_dict['source'] + "'})\n" + \
+                  "MATCH (t:Word {lemma: '" + arg_dict['target'] + "'})\n"
+        if prop_value is not None:
+            req += "CREATE (s)-[rel:" + arg_dict['type'] + " {" + arg_dict['prop'] + ": " + prop_value + "}]->(t)\n"
+        else:
+            req += "CREATE (s)-[rel:" + arg_dict['type'] + "]->(t)\n"
+        req += "RETURN rel"
+        result = session.run(req)
+    return result
 
 
 def update(arg_dict):
-    # TODO: cypher update
-    result = ''
+    node_type = arg_dict.get('type', 'Word')
+    node_prop = features[node_type][0]
+    prop = arg_dict['prop']
+    node_value = arg_dict['value'] if node_prop == 'id' else f"'{arg_dict['value']}'"
+    prop_value = arg_dict['prop-value'] if prop == 'id' else f"'{arg_dict['prop-value']}'"
+    req = "MATCH (x:" + node_type + " {" + node_prop + ": " + node_value + "})\n" + \
+          "SET x." + prop + " = " + prop_value + "\n"\
+          "RETURN x"
+    result = session.run(req)
     return result
 
 
 def search(arg_dict):
-    # TODO: cypher search
-    result = ''
+    node_type = arg_dict.get('type', 'Word')
+    prop = arg_dict['prop']
+
+    prop_value = arg_dict['prop-value'] if prop == 'id' else f"'{arg_dict['prop-value']}'"
+    req = "MATCH (x:" + node_type + " {" + prop + ": " + prop_value + "})\n"
+    if arg_dict['limit'] != 'no limit':
+        req += "LIMIT " + arg_dict['limit'] + "\n"
+    req += "RETURN x"
+    result = session.run(req)
     return result
 
 
 if __name__ == '__main__':
-    # driver = GraphDatabase.driver(
-    #     "bolt://54.237.90.173:33444",
-    #     auth=basic_auth("neo4j", "lockers-label-soldiers"))
-    # session = driver.session()
-
+    driver = GraphDatabase.driver(
+        "bolt://3.219.31.221:7687",
+        auth=basic_auth("neo4j", "petroleum-honor-flare"))
+    global session
+    session = driver.session()
     # un comment if move to heroku
     # port = int(os.environ.get("PORT", 5000))
     # app.run(host='0.0.0.0', port=port, debug=True)
